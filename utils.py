ff7ea61c4a143d46847554daa06b04af40ca646c
Erich Blume <blume.erich@gmail.com>
2014-02-01 03:37:32 -0800

    Better Redis connections and atomic id creation
    Redis connections are now shared from a persistent pool.
    This pool is threadsafe and process safe, so it doesn't
    matter what server runner you are using (WSGI, UWSGI,
    WerkZeug threading or processing or simple, apache, etc.),
    it will still work. These pools are accessed via a
    context manager to handle releasing the connections to avoid
    resource leaks.
    
    Removed baseN and tinyid functions and instead implemented
    make_id, which uses Redis' atomic INCR statement to keep track
    of an auto-incrementing int. This will be lost each time Redis is
    wiped, but that's probably the expected behavior anyway. This
    removes any possibility of a collision (and should be faster as
    well).

--- utils.py
+++ utils.py
@@ -1,24 +1,63 @@
-import os
-import time
 import base64
-import redis
 import config
+import contextlib
+import os
 import plivo
+import random
+import redis
+import urlparse
 
-def baseN(num,b,numerals="0123456789abcdefghijklmnopqrstuvwxyz"): 
-    return ((num == 0) and  "0" ) or (baseN(num // b, b).lstrip("0") + numerals[num % b])
 
-def tinyid(size=6):
-    id = '%s%s' % (
-            baseN(abs(hash(time.time())), 36), 
-            baseN(abs(hash(time.time())), 36))
-    return id[0:size]
+# Set up the redis pool, with some url parsing logic
+# See redis.client.StrictRedis.from_url for a reference
+#    (It would have been nice if py-redis exported from_url)
+def _make_redis_pool():
+    url = urlparse(config.REDIS_URL)
+    assert url.scheme == 'redis' or not url.scheme
+    try:
+        db = int(url.path.replace('/', ''))
+    except:
+        db = 0
+    return redis.BlockingConnectionPool(
+        max_connections = config.REDIS_MAX_CONNECTIONS,
+        timeout = config.REDIS_TIMEOUT,
+        host = url.hostname,
+        port = int(url.port or 6379),
+        db = db,
+        password = url.password,
+    )
+_pool = _make_redis_pool()
 
-def get_redis_connection():
-    redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-    rd = redis.from_url(redis_url)
-    return rd
+            
+def make_id(size=6):
+    """Generate a base-36 encoded string of the specified size.
+    
+    The string is gaurunteed to be unique since the last time redis was
+    rebooted. This uses redis' INCR command for an atomic
+    get-and-increment on an ID key, to avoid race conditions. As such,
+    this function is fully thread and process safe. Avoid using this if
+    you don't need that gauruntee.
+    """
+    # NB: Do we want to use capital letters? More traditional for Base36
+    alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
+    redis = get_redis_connection()
+    unique_val = redis.incr('ID_AUTO_INCR', 1)
+    rand = random.Random(unique_val)
+    return ''.join(random.choice(alphabet) for _ in range(size))
+
+
+@contextmanager
+def redis_client():
+    """Context manager that provides a redis client connection.
+    
+    This is thread-safe and process-safe. It may potentially block
+    for up to config.REDIS_TIMEOUT seconds, though (at which
+    point it will fail with an exception).
+    """
+    client = redis.StrictRedis(connection_pool=_pool)
+    yield
+    _pool.release(client)
+    
 
 def get_plivo_connection():
-    pl = plivo.RestAPI(config.PLIVO_AUTH_ID, config.PLIVO_AUTH_TOKEN)
-    return pl
+    return plivo.RestAPI(config.PLIVO_AUTH_ID, config.PLIVO_AUTH_TOKEN)
